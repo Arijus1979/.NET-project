a2ddf3b573994f087626e60a59b79292374270f3
Arijus Tamašauskas <arijus.tamasauskas@mif.stud.vu.lt>
2023-11-07 18:57:12 +0200

    Update0.1

---
+++ FinEngWeb/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using FinEngWeb.Data;
+using FinEngWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinEngWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name.ToLower()==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The category name and display order cannot be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The category has been created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The category has been updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "The category has been deleted successfully.";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FinEngWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using FinEngWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinEngWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Shoes", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "T-Shirts", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Pants", DisplayOrder = 3 },
+                new Category { CategoryId = 4, Name = "Hats", DisplayOrder = 4 }
+                );
+        }
+    }
+}
